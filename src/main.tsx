42b7e8bff163f85b740fd38e7d0875de73013444
yonzeraaa <s5.clanbr@gmail.com>
2025-04-02 16:35:51 -0300

    feat: Integrate Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext.tsx';
+import { SpeedInsights } from "@vercel/speed-insights/react" // Import SpeedInsights
 import './index.css';
 import App from './App.tsx';
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <SpeedInsights /> {/* Add SpeedInsights component */}
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
